fa838d782f217c44513cb328913729b56f0b9bb6
GustavoMont <lgmluisgm@gmail.com>
2021-08-20 06:18:34 -0300

    :pencil:prevent select's bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SelectCash from './Components/SelectCash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+    const [conteudo, setConteudo] = useState('Ola, mundo')
+    let moedaBase = 'BRL'
+    let moedaFinal = 'USD'
+    const seila = async () =>{
+        console.log(moedaBase, moedaFinal)
+        setConteudo ( await fetch(`https://economia.awesomeapi.com.br/json/${moedaBase}-${moedaFinal}`)
+        .then(respostaApi => respostaApi.json())
+        .then(resposta => `${resposta[0].name}`)
+        .catch(() => 'Ocorreu um erro. Perdão :(')
+        )
+        
+    }
+    return (
+        <>
+            <h1 onClick={seila}>{conteudo}</h1>
+            <SelectCash onChange={(e) =>{
+                if (e.target.value === moedaFinal) {
+                    const selectMoedaFinal = e.target.nextElementSibling 
+                    selectMoedaFinal.value = moedaBase
+                    moedaFinal = moedaBase
+                }
+                moedaBase = e.target.value
+                console.log(moedaBase, moedaFinal)
+            }}>
+                <option>BRL</option>
+                <option>USD</option>
+            </SelectCash>
+            <SelectCash onChange={(e) =>{
+                if (e.target.value === moedaBase) {
+                    const selectMoedaFinal = e.target.previousElementSibling
+                    selectMoedaFinal.value = moedaFinal
+                    moedaBase = moedaFinal
+                }
+                moedaFinal = e.target.value
+                console.log(moedaBase, moedaFinal)
+            }}>
+                <option>USD</option>
+                <option>BRL</option>
+            </SelectCash>
+        </>
+    )
 }
-
-export default App;
