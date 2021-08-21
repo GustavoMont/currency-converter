ec46d2356d42ad32cab303584ce7b294852a262e
GustavoMont <lgmluisgm@gmail.com>
2021-08-20 23:30:07 -0300

    :pencil: make select operation

--- src/App.js
+++ src/App.js
@@ -2,44 +2,58 @@ import React, { useState, useEffect } from 'react';
 import SelectCurrency from './Components/SelectCurrency';
 
 
+function Select(props){
+    return (
+        <SelectCurrency  className={props.className} 
+        onChange={e => props.change(e, props.oposit)}>
+                <option>{props.content[0]}</option>
+                <option>{props.content[1]}</option>
+                <option>{props.content[2]}</option>
+        </SelectCurrency>
+    )
+}
+
+
 export default function App(){
-    const [moedaBase, setMoedaBase] = useState('BRL')
-    const [moedaFinal, setMoedaFinal] = useState('USD')
+    const [moedaBase, setMoedaBase] = useState('USD')
+    const [moedaFinal, setMoedaFinal] = useState('BRL')
     const [resultado, setResultado] = useState('...')
+
+    const changeSelect = (e, oposit) =>{
+        const isMoedaFinal = !(oposit === 'moeda-final')
+        let troca = isMoedaFinal ? moedaFinal : moedaBase
+
+        const other = document.querySelector(`.${oposit}`)
+        if (e.target.value === other.value) {
+            if (isMoedaFinal) 
+                setMoedaBase(troca)
+            else 
+                setMoedaFinal(troca)
+            other.value = troca
+        }
+        if (isMoedaFinal) 
+            setMoedaFinal(e.target.value)
+        else 
+            setMoedaBase(e.target.value)
+    }
+
     useEffect( () => {
         setResultado('...')
         fetch(`https://economia.awesomeapi.com.br/json/${moedaBase}-${moedaFinal}`)
         .then(respostaApi => respostaApi.json())
         .then(resposta => resposta[0].high)
-        .then(currency => setResultado(parseFloat(currency).toLocaleString(moedaFinal)))
-
+        .then(currency => setResultado(parseFloat(currency).toLocaleString(moedaFinal))) 
+          
     }, [moedaBase, moedaFinal])
     
     return (
         <>
             <h1>{resultado}</h1>
-            <SelectCurrency onChange={e => {
-                const selectMF = document.querySelector('.moeda-final')
-                if (e.target.value === selectMF.value) {
-                    selectMF.value = moedaBase
-                    setMoedaFinal(moedaBase)                 
-                }
-                setMoedaBase(e.target.value)
-            }} className="moeda-base">
-                <option>BRL</option>
-                <option>USD</option>
-            </SelectCurrency>
-            <SelectCurrency className="moeda-final" onChange={e =>{
-                const selectMB = document.querySelector('.moeda-base')
-                if (e.target.value === selectMB.value) {
-                    selectMB.value = moedaFinal
-                    setMoedaBase(moedaFinal)                 
-                }
-                setMoedaFinal(e.target.value)
-            }}>
-                <option>USD</option>
-                <option>BRL</option>
-            </SelectCurrency>
+            
+            <Select className="moeda-base" change={changeSelect} 
+            oposit={'moeda-final'} content={['USD', 'BRL', 'EUR']} /> 
+            <Select className="moeda-final" change={changeSelect} 
+            oposit={'moeda-base'} content={['BRL', 'USD', 'EUR']}/>
         </>
     )
 }
