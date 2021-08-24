3ffa5b5696ee78055f9d7adc14c2771b094a3aae
GustavoMont <lgmluisgm@gmail.com>
2021-08-24 10:06:50 -0300

    :pencil2: costumize selectcurrency

--- src/Components/SelectCurrency/index.js
+++ src/Components/SelectCurrency/index.js
@@ -2,6 +2,15 @@ import styled from 'styled-components';
 
 
 const SelectCurrency = styled.select`
-    background-color: #00ff00;
+    width: 300px;
+    max-width: 100%;
+
+    font-size: 1.2rem;
+
+    padding: .8rem 1rem;
+    margin-top: 2rem;
+
+    background-color: ${props => props.className === 'moeda-final'? '#00ff37' : '#e83e33'};
+
 `
 export default SelectCurrency;
